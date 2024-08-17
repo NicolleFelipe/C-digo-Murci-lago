9ad8f4ba9856bab9797c7c9d599deadac8f84ffc
Nicolle Felipe <nfeliper1@miumg.edu.gt>
2024-08-16 20:31:55 -0600

    Add files via upload

---
+++ Traductor/src/visual/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package visual;
+
+/**
+ *
+ * @author nicolle
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_Murcielago frm = new frm_Murcielago();
+        frm.show();
+        // TODO code application logic here
+    }
+    
+}
